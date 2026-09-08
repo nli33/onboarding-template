bc4bc999ae0910321beed12f0b50be8cf0cc2c41
nli33 <domesticatedhuman9999@gmail.com>
2026-09-08 15:32:04 -0400

    imprpve test script

--- avg.py
+++ avg.py
@@ -3,9 +3,19 @@ import json
 import subprocess
 import sys
 
+BUILD = 'cmake --build --preset benchmark'.split()
+TEST = 'ctest --preset benchmark --output-on-failure'.split()
 BENCH = './build/benchmark/uwhpc_benchmark'
 
 if __name__ == '__main__':
+    build = subprocess.run(BUILD)
+    if build.returncode != 0:
+        sys.exit(build.returncode)
+        
+    test = subprocess.run(TEST)
+    if test.returncode != 0:
+        sys.exit(test.returncode)
+    
     totals = defaultdict(float)
     try:
         count = int(sys.argv[1])
